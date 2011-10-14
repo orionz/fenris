781aafda60dab8c292c39773ac96b4becc113ae9
Orion Henry <orion@heroku.com>
2011-10-13 21:12:52 -0700

    got SSL working between the endpoints

--- lib/chairman/em.rb
+++ lib/chairman/em.rb
@@ -1,12 +1,32 @@
 require 'eventmachine'
 
 module Chairman
+
+  module Uber
+    def initialize(client, ssl)
+      @client, @ssl = client, ssl
+    end
+  end
+
   module ProxyConnection
-    def initialize(client)
-      @client = client
+    def initialize(client, ssl)
+      @client, @ssl = client, ssl
     end
 
     def post_init
+      if (@ssl)
+        start_tls :private_key_file => '/tmp/client.key', :cert_chain_file => '/tmp/client.crt', :verify_peer => true
+      else
+        @client.enable_proxy self
+      end
+    end
+
+    def ssl_verify_peer(cert)
+      authority_key = OpenSSL::PKey::RSA.new File.read("/tmp/authority.pub")
+      @verified ||= OpenSSL::X509::Certificate.new(cert).verify(authority_key)
+    end
+
+    def ssl_handshake_completed
       @client.enable_proxy self
     end
 
@@ -18,39 +38,36 @@ module Chairman
       @client.close_connection_after_writing
     end
   end
-end
 
-module Chairman
-  module ClientSocket
-    def initialize(host, port)
+  module ProviderSocket
+    def initialize(host, port, ssl)
       @host, @port = host, port
+      @ssl = ssl
+      @q = []
     end
 
     def post_init
-      EventMachine::connect @host, @port, Chairman::ProxyConnection, self
+      if (@ssl)
+        start_tls :private_key_file => '/tmp/server.key', :cert_chain_file => '/tmp/server.crt', :verify_peer => true
+      else
+        EventMachine::connect @host, @port, Chairman::ProxyConnection, self, true
+      end
     end
 
-    def receive_data data
+    def ssl_verify_peer(cert)
+      authority_key = OpenSSL::PKey::RSA.new File.read("/tmp/authority.pub")
+      @verified ||= OpenSSL::X509::Certificate.new(cert).verify(authority_key)
     end
-  end
-end
 
-module Chairman
-  module ServiceSocket
-    def initialize(host, port)
-      @host, @port = host, port
-      @q = []
+    def ssl_handshake_completed
+      EventMachine::connect @host, @port, Chairman::ProxyConnection, self, false unless @unbound
     end
 
     def enable_proxy(dest)
       @q.each { |d| dest.send_data d }
       @q = []
       @target = dest
-      EventMachine::enable_proxy dest, self
-    end
-
-    def post_init
-      EventMachine::connect @host, @port, Chairman::ProxyConnection, self
+      EventMachine::enable_proxy dest, self unless @unbound
     end
 
     def receive_data data
@@ -60,22 +77,25 @@ module Chairman
         @q << data
       end
     end
-  end
-end
 
-module Chairman
+    def proxy_target_unbound
+      close_connection
+    end
+
+    def unbind
+      @unbound = true
+      @client.close_connection_after_writing if @client
+    end
+  end
+
   module Base
     extend self
-
-    def url
-      ENV['CHAIRMAN_URL'] || (raise "define CHAIRMAN_URL")
-    end
 
     def serve(client, from, to)
       EventMachine::run do
         client.update("0.0.0.0", from)
         puts "Serving port #{to} on #{from}"
-        EventMachine::start_server "0.0.0.0", from, Chairman::ServiceSocket, "127.0.0.1", to
+        EventMachine::start_server "0.0.0.0", from, Chairman::ProviderSocket, "127.0.0.1", to, true
       end
     end
 
@@ -83,20 +103,22 @@ module Chairman
       Dir.chdir(dir)
 
       at_exit do
-        client.services.each do |service|
-          puts "Deleting socket '#{service.name}'."
-          File.delete service.name if File.exists? service.name
+        client.providers.each do |provider|
+          puts "Deleting socket '#{provider["name"]}'."
+          File.delete provider["name"] if File.exists? provider["name"]
         end
       end
 
       EventMachine::run do
-        client.services.each do |service|
-          puts "Making socket '#{service["name"]}'."
-          EventMachine::start_unix_domain_server service["name"], Chairman::ClientSocket, service["server_ip"], service["server_port"].to_i
+        client.providers.each do |provider|
+          puts "Making socket '#{provider["binding"]}'."
+          puts provider.inspect
+          if provider["ip"]
+            EventMachine::start_unix_domain_server provider["binding"], Chairman::ProviderSocket, provider["ip"], provider["port"].to_i, false
+          end
         end
       end
     end
-
   end
 end
 
